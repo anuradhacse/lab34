497d7df5b8ddab45202fb6c183446744446cd196
anuradha <niroshonmora@gmail.com>
2017-07-22 18:56:47 +0530

    intial commit

---
+++ main.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+#include <cfloat>
+
+using namespace std;
+
+float** matrixGenarator();
+void sequentialMultiplication(int,float,float);
+int sizeOfTheMatrix(int,int);
+int mean();
+int std();
+int sampleSize();
+float multiply(int);
+
+int main() {
+
+    int intial_matrix_size=200;
+    int steps = 200;
+    int n;
+    for(int i=0; i<=steps; ++i){
+        n = sizeOfTheMatrix(intial_matrix_size, i);
+       multiply(n);
+    }
+    cout<< "final matrix size : "<<n;
+
+    for(int i=0; i<n; ++i){
+        for(int j=0; j<n; ++i){}
+    }
+
+    return 0;
+}
+
+/*
+ * Function which take two matrices as arguments and calculate its product sequentially. rowCount=columnCount=n
+ */
+void sequentialMultiplication(int n,float** matrixA, float** matrixB){
+
+}
+
+/*Return a nxn matrix filled with random values.seed the generator with the system time then output a single random number,
+ which should be different each time we run the program*/
+float** matrixGenarator(int n){
+
+        //Initializing a nxn matrix
+        float** matrix = new float*[n];
+        for(int i = 0; i < n; ++i)
+            matrix[i] = new float[n];
+
+        //filling random values to matrix
+        for(int i=0; i<n; ++i){
+            for(int j=0; j<n; ++j){
+                //randomizing seed
+                srand((unsigned)time(0));
+                //This will generate a number from 0.0 to FLOAT_MAX:
+                float randomNumber = static_cast <float> (rand()) / (static_cast <float> (RAND_MAX/FLT_MAX));
+
+                matrix[i][j] = randomNumber;
+
+        }
+
+    }
+
+}
+
+float multiply(int matrixSize){
+    float** matrixA= matrixGenarator(matrixSize);
+    float** matrixB= matrixGenarator(matrixSize);
+
+    clock_t tStart = clock();
+
+    sequentialMultiplication(matrixSize,matrixA,matrixB);
+
+    float execution_time = (float)(clock() - tStart)/CLOCKS_PER_SEC;
+
+    return execution_time;
+
+}
+
+
+int sizeOfTheMatrix(int n, int i)
+{
+    return n+ 9*i;
+}
