a9d81b0f965c464cb17013d56d3147078e4b3440
anuradha <niroshonmora@gmail.com>
2017-07-22 22:00:21 +0530

    sequential part completed

--- main.cpp
+++ main.cpp
@@ -1,34 +1,31 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <cmath>
 #include <string>
 #include <cfloat>
 
 using namespace std;
 
-float** matrixGenarator();
-void sequentialMultiplication(int,float,float);
+float** matrixGenarator(int);
+void sequentialMultiplication(int,float**,float**, float**);
 int sizeOfTheMatrix(int,int);
-int mean();
-int std();
-int sampleSize();
+float mean(int, float*);
+float standardDeviation(int, float, float*);
+float correctSampleSizeCalculator(float,float, float, float);
 float multiply(int);
-void runMultiply();
+void runMultiply(int,int);
 
 int main() {
 
-    int intial_matrix_size=200;
-    int steps = 200;
-    int n;
-    for(int i=0; i<=steps; ++i){
-        n = sizeOfTheMatrix(intial_matrix_size, i);
-       multiply(n);
+    int matrix_size=200;
+    int sample_size = 50;
+    for(int i=0; i<10; ++i){
+        matrix_size = sizeOfTheMatrix(matrix_size,i);
+       runMultiply(matrix_size,sample_size);
     }
-    cout<< "final matrix size : "<<n;
+    cout<< "final matrix size : "<<matrix_size;
 
-    for(int i=0; i<n; ++i){
-        for(int j=0; j<n; ++i){}
-    }
 
     return 0;
 }
@@ -36,12 +33,13 @@ int main() {
 /*
  * Function which take two matrices as arguments and calculate its product sequentially. rowCount=columnCount=n
  */
-void sequentialMultiplication(int n,float** matrixA, float** matrixB){
+void sequentialMultiplication(int n,float** matrixA, float** matrixB, float** matrixC){
 
     for(int i=0; i<n ;++i){
         for(int j=0; j<n; ++j){
+            matrixC[i][j]=0;
             for(int k=0; k<n; ++k){
-                matrixA[i][k]*matrixB[k][j];
+                matrixC[i][j] += matrixA[i][k]*matrixB[k][j];
 
             }
         }
@@ -71,29 +69,36 @@ float** matrixGenarator(int n){
                 matrix[i][j] = randomNumber;
 
         }
-
+            return matrix;
     }
 
 }
 
-void runMultiply(int matrixSize){
+void runMultiply(int matrixSize, int sampleSize){
 
-    int sampleSize = 50;
-    float sum_of_execution_times = 0;
+    float execution_time = 0;
+    float mean_val = 0,standardDeviation_val=0,r_value=5,z_value=1.960,correctSampleSize=0;
+    float* execution_time_array = new float[sampleSize]();
     for(int i=0; i<sampleSize; ++i){
-         sum_of_execution_times = sum_of_execution_times + multiply(matrixSize);
-
+      execution_time = multiply(matrixSize);
+        execution_time_array[i] = execution_time;
     }
+    mean_val = mean(sampleSize, execution_time_array);
+    standardDeviation_val = standardDeviation(sampleSize,mean_val,execution_time_array);
+    correctSampleSize = correctSampleSizeCalculator(z_value,r_value, standardDeviation_val, mean_val);
+
+    cout<< "correct sample size : "<<correctSampleSize<<"for matrix size :"<<matrixSize<<endl;
 }
 
 
 float multiply(int matrixSize){
-    float** matrixA= matrixGenarator(matrixSize);
-    float** matrixB= matrixGenarator(matrixSize);
+    float** matrixA = matrixGenarator(matrixSize);
+    float** matrixB = matrixGenarator(matrixSize);
+    float** matrixC = matrixGenarator(matrixSize);
 
     clock_t tStart = clock();
 
-    sequentialMultiplication(matrixSize,matrixA,matrixB);
+    sequentialMultiplication(matrixSize,matrixA,matrixB,matrixC);
 
     float execution_time = (float)(clock() - tStart)/CLOCKS_PER_SEC;
 
@@ -101,8 +106,32 @@ float multiply(int matrixSize){
 
 }
 
+float correctSampleSizeCalculator(float z_value,float r_value,float std,float mean){
 
-int sizeOfTheMatrix(int n, int i)
+    float n = (100*z_value*std)/(r_value*mean);
+    return pow(n,2);
+
+}
+
+int sizeOfTheMatrix(int n,int i)
 {
-    return n+ 9*i;
+    return n+ 200*i;
+}
+
+float mean(int sampleSize, float* ExecutionTimeArray){
+    float totalExecutionTime = 0;
+    for(int i=0;i<sampleSize;++i){
+        totalExecutionTime += ExecutionTimeArray[i];
+    }
+
+    return totalExecutionTime/sampleSize;
+}
+
+float standardDeviation(int sampleSize, float mean, float* ExecutionTimeArray){
+    float deviation = 0;
+    float std;
+    for(int i=0;i<sampleSize;++i){
+        deviation += pow(mean - ExecutionTimeArray[i],2)/sampleSize;
+    }
+    return sqrt(deviation);
 }
